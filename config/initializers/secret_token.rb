e336dabbc5574a46c4793e5a5ae9e83484ffcaee
Kris-Mikael Krister <krismikael@gmail.com>
2012-02-06 21:36:42 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mkm3::Application.config.secret_token = '4b08853d1fe1572ffbb62f2bc8bf5d4e071c5cc10eb7e297f62cb2027edca3660fee32b4120cd8a4d0d8bf7d6fdc69e82febedd1831eb222fb0a48af993e0eb1'
