271262aed752d48516f28326e9d859d81404cf1b
Kris-Mikael Krister <krismikael@gmail.com>
2012-03-26 20:42:43 +0200

    should also navigate without backbone history

--- public/backbone_pushstate_router.js
+++ public/backbone_pushstate_router.js
@@ -1,22 +1,19 @@
 // Use absolute URLs  to navigate to anything not in your Router.
 $(function() {
-  // Only need this for pushState enabled browsers
-  if (Backbone.history && Backbone.history._hasPushState) {
 
-    // Use delegation to avoid initial DOM selection and allow all matching elements to bubble
-    $(document).delegate("a", "click", function(evt) {
-      // Get the anchor href and protcol
-      var href = $(this).attr("href");
-      var protocol = this.protocol + "//";
+  // Use delegation to avoid initial DOM selection and allow all matching elements to bubble
+  $(document).delegate("a", "click", function(evt) {
+    // Get the anchor href and protcol
+    var href = $(this).attr("href");
+    var protocol = this.protocol + "//";
 
-      // Only internal links are interesting
-      if ((href.match(/\//) || href.match(/\/\w+/)) && !href.match(/^http/) && !href.match(/^#/)) {
+    // Only internal links are interesting
+    if ((href.match(/\//) || href.match(/\/\w+/)) && !href.match(/^http/) && !href.match(/^#/)) {
 
-        // Stop the event bubbling to ensure the link will not cause a page refresh.
-        evt.preventDefault();
-        mkm.routers.router.navigate(href, true);
-      }
-    });
+      // Stop the event bubbling to ensure the link will not cause a page refresh.
+      evt.preventDefault();
+      mkm.routers.router.navigate(href, true);
+    }
+  });
 
-  }
 });
