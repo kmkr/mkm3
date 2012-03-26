929e33e2d7b76dbb9489f7068eafa87aa0cb10da
Kris-Mikael Krister <krismikael@gmail.com>
2012-03-26 21:46:36 +0200

    better css

---
+++ makeStyles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ROOT=~/mkm3
+
+cd $ROOT/vendor/assets/stylesheets/bootstrap
+lessc bootstrap.less > $ROOT/app/assets/stylesheets/libs/bootstrap.css
+sed 's/@/$/g' variables.less > $ROOT/app/assets/stylesheets/variables.css.scss
+sed -i '/spin/d' $ROOT/app/assets/stylesheets/variables.css.scss
+cd $ROOT
