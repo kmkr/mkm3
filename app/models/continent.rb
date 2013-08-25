5fd2eee16705a77f0eafc5997d930d52be483e24
Kris-Mikael Krister <krismikael@gmail.com>
2013-08-25 21:35:50 +0200

    added attr_accessible to rest of models

--- app/models/continent.rb
+++ app/models/continent.rb
@@ -1,3 +1,4 @@
 class Continent < ActiveRecord::Base
   has_many :countries
+  attr_accessible :title, :priority
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,5 @@
 class Country < ActiveRecord::Base
   belongs_to :continent
   has_many :articles
+  attr_accessible :title, :latitude, :longitude, :zoom_level, :continent_id
 end
