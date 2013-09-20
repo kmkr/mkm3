432fab19d5d88af8e40056bbc78b8820a91b09a1
Kris-Mikael Krister <krismikael@gmail.com>
2013-09-20 23:41:27 +0200

    added log statement for upload

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -13,7 +13,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}s/article_#{model.article.id}/#{model.id}"
+    src = "uploads/#{model.class.to_s.underscore}s/article_#{model.article.id}/#{model.id}"
+    logger.info "Uploading to #{src}"
+    src
   end
 
   def cache_dir
