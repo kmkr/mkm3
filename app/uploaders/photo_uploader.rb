dccf0938bca8b946c052637243b0a16f8617206b
Kris-Mikael Krister <krismikael@gmail.com>
2013-09-21 00:00:28 +0200

    logging before NPE

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -13,7 +13,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    Rails.logger.info "Trying to store #{model.inspect} on #{article.inspect}"
+    Rails.logger.info "Trying to store #{model.inspect}"
     "uploads/#{model.class.to_s.underscore}s/article_#{model.article.id}/#{model.id}"
   end
 
